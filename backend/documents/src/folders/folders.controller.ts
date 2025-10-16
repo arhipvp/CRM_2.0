adcd7fc808f720649a8ce80413090fb969baa8ad
arhipvp <112897986+arhipvp@users.noreply.github.com>
2025-10-16 13:26:43 +0200

    refactor: expose folder and permissions responses

--- backend/documents/src/folders/folders.controller.ts
+++ backend/documents/src/folders/folders.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 import { CreateFolderDto } from './dto/create-folder.dto';
+import { FolderResponse } from './dto/folder-response.dto';
 import { FindFolderParamsDto } from './dto/find-folder-params.dto';
 import { FoldersService } from './folders.service';
 
@@ -9,12 +10,12 @@ export class FoldersController {
   constructor(private readonly foldersService: FoldersService) {}
 
   @Post()
-  async create(@Body() dto: CreateFolderDto) {
+  async create(@Body() dto: CreateFolderDto): Promise<FolderResponse> {
     return this.foldersService.create(dto);
   }
 
   @Get(':ownerType/:ownerId')
-  findOne(@Param() params: FindFolderParamsDto) {
+  findOne(@Param() params: FindFolderParamsDto): Promise<FolderResponse> {
     return this.foldersService.findByOwner(params.ownerType, params.ownerId);
   }
 }
